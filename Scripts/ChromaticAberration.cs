286a1b0cbdc5159e41b9b37e35cbdd5922220728
Bruno Araujo <araujo.contato@gmail.com>
2021-05-30 01:46:46 -0300

    fix: small tweaks to version 2020.3.0f

--- Scripts/ChromaticAberration.cs
+++ Scripts/ChromaticAberration.cs
@@ -2,43 +2,35 @@
 using System.Collections;
 
 [ExecuteInEditMode]
-public class ChromaticAberration : MonoBehaviour {
-	private Shader shader;
-	private Material material;
-	[Range(0.0f, 1.0f)]
-	public float chromaticAberration = 1.0f;
+public class ChromaticAberration : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float chromaticAberration = 1.0f;
     public bool onTheScreenEdges = true;
 
-	public void Start() {
-		shader = new Shader();
-		shader = Shader.Find("Hidden/ChromaticAberration");
-		material = new Material(shader);
+    private Shader shader;
+    private Material material;
 
-		if (!SystemInfo.supportsImageEffects) {
-			enabled = false;
-			return;
-		}
+    public void Start()
+    {
+        shader = Shader.Find("Hidden/ChromaticAberration");
+        material = new Material(shader);
 
-		if (!shader && !shader.isSupported) {
-			enabled = false;
-		}
-	}
+        if (!shader && !shader.isSupported) {
+            enabled = false;
+        }
+    }
 
-	public void OnRenderImage(RenderTexture inTexture, RenderTexture outTexture) {
-		if (shader != null) {
-			material.SetFloat("_ChromaticAberration", 0.01f * chromaticAberration);
+    public void OnRenderImage(RenderTexture inTexture, RenderTexture outTexture)
+    {
+        if (shader == null) {
+            Graphics.Blit(inTexture, outTexture);
+            return;
+        }
 
-            if (onTheScreenEdges)
-				material.SetFloat("_Center", 0.5f);
-
-            else
-				material.SetFloat("_Center", 0);
-
-            Graphics.Blit(inTexture, outTexture, material);
-		}
-		else {
-			Graphics.Blit (inTexture, outTexture);
-		}
-	}
+        material.SetFloat("_ChromaticAberration", 0.01f * chromaticAberration);
+        material.SetFloat("_Center", onTheScreenEdges ? 0.5f : 0);
+        Graphics.Blit(inTexture, outTexture, material);
+    }
 }
 
